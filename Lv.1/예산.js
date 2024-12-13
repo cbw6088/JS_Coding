d52a150ad789c37da58e8ada7f9d2dc2f780fdb7
cbw6088 <cbw4186@naver.com>
2024-12-13 16:35:24 +0900

    예산

---
+++ Lv.1/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    var answer = 0;
+    d.sort((a, b) => a - b);
+    for(i=0; i<d.length; i++){
+        if(d[i] > budget) break;
+        
+        budget -= d[i];
+        answer++;
+    }
+    return answer;
+}
