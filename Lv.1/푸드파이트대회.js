428f187c088e0820a5d56cafa79b4dff3e3e7d49
cbw6088 <cbw4186@naver.com>
2024-12-20 23:30:29 +0900

    푸드파이트대회

--- Lv.1/푸드파이트대회.js
+++ Lv.1/푸드파이트대회.js
@@ -1,9 +1,7 @@
 function solution(food) {
     let arr = [];
     food.map((e, i) => {
-      if (e > 1) {
-        arr.push(i.toString().repeat(Math.floor(e / 2)));
-      }
+      if (e > 1)  arr.push(i.toString().repeat(Math.floor(e / 2)));
     });
     return arr.join("") + "0" + arr.reverse().join("");
   }
